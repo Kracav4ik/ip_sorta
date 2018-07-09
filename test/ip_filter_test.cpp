8c6e580fb8fc1a9b70cebbf2c2aecb4329b30bb4
Kracav4ik <dikama2013@yandex.ru>
2018-07-09 22:07:09 +0300

    added travis and testing

---
+++ test/ip_filter_test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+TEST(ip_sorta_tests, is_valid) {
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
